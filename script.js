13374d3a1f26fe27b9c74eb3555e1bade85fc9ac
Vinigperuzzi <vinigperuzzi@hotmail.com>
2024-03-31 19:56:01 -0300

    Update: all features done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function printData(){
   let result = await fetch('https://swapi.dev/api/planets/?format=json')
   let {results} = await result.json()
   console.log(results);
-  results.forEach((planet, index) => {
+  results.forEach(planet => {
     let li = document.createElement('li');
     li.innerHTML = `<button onclick="getData('${planet['name']}')">${planet['name']}</button>`;
     planetsDiv.appendChild(li)
@@ -12,7 +12,7 @@ async function printData(){
 
 async function getData(planetName){
   const planetResults = document.getElementById('show-results');
-  const tableBody = document.getElementById('table-body');
+  const tableResults = document.getElementById('table-results');
   if(!planetName){
     const search_box = document.getElementById('search-box');
     planetName = search_box.value;
@@ -33,6 +33,7 @@ async function getData(planetName){
     return;
   }
   p.innerHTML = `
+                <h1>Informações do planeta ${planetInfo['name']}</h1>
                 <p>
                   <p>Nome = ${planetInfo['name']}</p>
                   <p>Clima = ${planetInfo['climate']}</p>
@@ -41,8 +42,41 @@ async function getData(planetName){
                 </p>
                 `;
   planetResults.appendChild(p);
-  residents = planetInfo['residents'];
-  console.log(residents);
+  
+  residentsURL = planetInfo['residents'];
+
+  if (residentsURL.length === 0){
+    tableResults.innerHTML = '<p>Não há habitantes famosos que tenham vindo desse planeta!</p>';
+    return
+  }
+
+  tableResults.innerHTML = `
+                        <h1>Habitantes mais famosos do planeta ${planetInfo['name']}</h1>
+                        <table border="4">
+                          <thead>
+                            <tr>
+                              <th>Nome</th>
+                              <th>Data de nascimento</th>
+                            </tr>
+                          </thead>
+                          <tbody id="table-body">
+
+                          </tbody>
+                        </table>
+                        `;
+
+  residentsURL.forEach(resident => getResident(resident));
+
+  async function getResident(resident){
+    residentInfo = await (await fetch(resident)).json();
+    const tableBody = document.getElementById('table-body');
+    let tr = document.createElement('tr');
+    tr.innerHTML = `
+                      <th>${residentInfo['name']}</th>
+                      <th>${residentInfo['birth_year']}</th>
+                    `;
+    tableBody.appendChild(tr);
+  }
 }
 
 printData();
